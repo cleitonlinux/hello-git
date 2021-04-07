fccf97f38e0972b59c78ab32c9fbbc3aff7be4da
Cleiton Barbosa <cleiton.cbl@hotmail.com>
2021-04-07 19:54:07 +0000

    Script agnóstico

--- main.sh
+++ main.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+source /etc/os-release
 
-apt-get update
-apt-get install -y --simulate mariadb-server
+
+function _debian {
+ 	dkgp -la | grep mariadb-server > /dev/null
+	test $? -eq 0 && return
+	apt-get update && apt-get install -y --simulate mariadb-server
+}
+
+function _centos {
+	rpm -qa | grep mariadb-server > /dev/null
+	teste $:? eq 0 && return
+	dnf install -y mariadb-server
+
+}
+
+_$ID
